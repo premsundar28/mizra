8d74a6d3478254eb0fbb42cb4626a6a18e8764b5
premsundar28 <108226805+premsundar28@users.noreply.github.com>
2024-09-28 10:33:49 +0530

    gateway

--- User/src/main/java/org/example/User.java
+++ User/src/main/java/org/example/User.java
@@ -10,7 +10,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class User {
     public static void main(String[] args) {
 
-        SpringApplication.run(User.class, args);
+         SpringApplication.run(User.class, args);
 
     }
 }
---
+++ api-gayeway/src/main/java/org/example/ApiGatewayApplication.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class,args);
+    }
+}
